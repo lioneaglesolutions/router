7a78b6da9e59696b12e888873aa20597099d09b4
James Mooring <james@lioneagle.solutions>
2023-08-20 16:12:52 +1000

    wip

--- examples/vue/basic/src/App.vue
+++ examples/vue/basic/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from '@tanstack/vue-router'
+import { HelloWorld } from '@tanstack/vue-router'
 </script>
 
 <template>
@@ -11,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld />
 </template>
 
 <style scoped>
